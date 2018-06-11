2f85e9294c986e77acea8184120edec56c3c462b
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-11 16:29:45 -0300

    adicionei algumas dependencias

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const { Remessa } = require('./Remessa');
+const express = require('express');
+
+const app = express();
 
 const rem = new Remessa(fs.readFileSync('./CNAB240_1630_32135_050418_268.REM'));
 
