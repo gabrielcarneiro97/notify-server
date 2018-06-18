ac721037d86942d6aa1cbf24a9589731fb2164ec
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-18 14:10:09 -0300

    Troquei a porta do main

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const { SSL } = require('./services');
 
 if (process.argv[2] === 'ssl') {
   console.log(SSL);
-  https.createServer(SSL, app).listen(8181, () => {
+  https.createServer(SSL, app).listen(8080, () => {
     console.log('SSL server listening 8181 port');
   });
 } else {
-  const server = app.listen(8181, () => {
+  const server = app.listen(8080, () => {
     const { address, port } = server.address();
     console.log(`App listening at http://${address}:${port}`);
   });
